3bda94e04b0966408971eafe9df2feae1d7c3dd0
Frxhb <huber.frani@gmail.com>
2021-09-21 00:00:31 +0200

    edited tkinter

--- youtube-dl-python.py
+++ youtube-dl-python.py
@@ -1,11 +1,9 @@
 import os
 import time
-import subprocess
+import glob
 
 from tkinter import *
 import tkinter as tk
-import string
-from typing import Collection
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,106 +16,149 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def main_programm():
-    clear = lambda: os.system('clear')
-    clear()
-    def re_run():
-        clear()
-        ask_re_run = input("Re run? Y/n:\n>>> ")
-        if ask_re_run in ['yes', 'Yes', 'Y', 'y']:
-            main_programm()
-        else:
-            print("Exiting program...")
-            time.sleep(1)
-            exit()
-
-    def youtubedl_video():
-        os.system('youtube-dl ' + URL_Abfrage)
-        print("\n\n")
-
-    def youtubedl_audio():
-        os.system('youtube-dl -x --audio-format best ' + URL_Abfrage)
-        print("\n\n")
-
-    URL_Abfrage = input("Which URL? Paste URL and press enter.\n>>> ")
-
-    Type_Abfrage = input("Video [1] or Audio [2] ?\n>>> ")
-
-    if Type_Abfrage == "1":
-        print("Okay, you chosed Video.")
-        youtubedl_video()
-
-    elif Type_Abfrage == "2":
-        print("Okay, you chosed Audio.")
-        youtubedl_audio()
-
-    else:
-        print(bcolors.WARNING + "Falsche Eingabe!\n" + bcolors.ENDC)
-
-    re_run()
-
-#main_programm()
-
 
 def close_window(): 
     root.destroy() 
 
 def retrieve_input():
     global inputValue
-    inputValue=text2.get("1.0","end-1c")
+    inputValue=textbox1.get("1.0","end-1c")
 
 
 def video_download():
-    os.popen('youtube-dl ' + inputValue)
+    os.system('youtube-dl ' + inputValue)
 
 def audio_download():
-    os.popen('youtube-dl ' + inputValue)    
+    os.system('youtube-dl ' + inputValue)    
+
+'''
+def show_url():
+    textbox3.configure(state='normal')
+    textbox3.delete('1.0', END)
+    textbox3.insert(END, inputValue)
+    textbox3.configure(state='disabled')
+'''
+
+def show_video_files():
+    textbox2.configure(state='normal')
+    textbox2.delete('1.0', END)
+    textbox2.configure(state='disabled')
+    initial_count = 0
+    direc = ('./')
+    files = os.listdir(direc)
+    os.chdir(direc)
+    textbox2.delete('1.0', END)
+    for file in (glob.glob("*.mp4")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')
+        textbox2.configure(state='disabled')
+
+    for file in (glob.glob("*.webm")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')    
+        textbox2.configure(state='disabled')
 
 
-def get_work_output_video():
-    out = subprocess.run(["youtube-dl", inputValue], capture_output=True)
-    text3.insert(END, out)
+def show_audio_files():
+    textbox2.configure(state='normal')
+    textbox2.delete('1.0', END)
+    textbox2.configure(state='disabled')
+    initial_count = 0
+    direc = ('./')
+    files = os.listdir(direc)
+    os.chdir(direc)
+    for file in (glob.glob("*.aac")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')
+        textbox2.configure(state='disabled')
+    
+    for file in (glob.glob("*.m4a")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')
+        textbox2.configure(state='disabled')
 
-def get_work_output_audio():
-    out = subprocess.run(["youtube-dl" , "-x" ,  "--audio-format",  "best", inputValue], capture_output=True)
-    text3.insert(END, out)
+    for file in (glob.glob("*.mkv")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')
+        textbox2.configure(state='disabled')
+
+    for file in (glob.glob("*.mp3")):
+        initial_count += 1
+        textbox2.configure(state='normal')
+        textbox2.insert(END, file + '\n\n\n')    
+        textbox2.configure(state='disabled')
 
 
-######TK
+#Tk definition begins here:
 
 root = Tk()
 root.title("ADB GUI")
-root.geometry("900x700")
+root.geometry("1100x700")
 #root.eval('tk::PlaceWindow . center')
 
-btn1 = Button(root, text="Video", command=get_work_output_video)
-#=video_download)
-btn1.grid(row=1, column=1)
 
-btn2 = Button(root, text="Audio",command=get_work_output_audio)
-#=audio_download)
-btn2.grid(row=1, column=2)
+textbox1 = Text(root, height = 3, width = 40)
+textbox1.grid(row=1, column=0, sticky="NW")
+textbox1.configure(state='normal')
 
-btn3 = Button(root, text="Cancel",command=close_window)
-btn3.grid(row=3, column=3)
+textbox2 = Text(root, height = 20, width = 75)
+textbox2.grid(row=3, column=0,sticky="NW")
+textbox2.configure(state='disabled')
 
-buttonCommit=Button(root, height=1, width=10, text="Commit", 
-                    command=lambda: retrieve_input())
-buttonCommit.grid(row=3, column=0)
+textbox3 = Text(root, height = 3, width = 40)
+textbox3.grid(row=5, column=0,sticky="NW")
+textbox3.configure(state='disabled')
 
-
-text2 = Text(root, height = 2, width = 30)
-text2.grid(row=1, column=0)
-text2.configure(state='normal')
-
-text3 = Text(root)
-text3 = Text(root, height = 10, width = 50)
-text3.grid(row=2, column=0)
-#text3.insert(END, output)
+def show_url_box():
+    textbox3.configure(state='normal')
+    textbox3.delete('1.0', END)
+    textbox3.insert(END, inputValue)
+    textbox3.configure(state='disabled')
 
 
-lbl_phone_status = Label(root, font=("times", 15))
-lbl_phone_status.grid(row=0, column=0)
-lbl_phone_status.config(text="URL")
+btn1 = Button(root, text="Video", command=video_download)
+btn1.grid(row=1, column=2)
+
+btn2 = Button(root, text="Audio",command=audio_download)
+btn2.grid(row=1, column=3)
+
+btn3 = Button(root, text="Cancel", bg="red", fg="white", command=close_window)
+btn3.grid(row=5, column=3, sticky="SE")
+
+btn4 = Button(root, text="Show Video Files",command=show_video_files)
+btn4.grid(row=2, column=2, sticky="E")
+
+btn5 = Button(root, text="Show Audio Files",command=show_audio_files)
+btn5.grid(row=2, column=3, sticky="E")
+
+'''
+btn6 = Button(root, text="Show URL",command=show_url)
+btn6.grid(row=4, column=3, sticky="E")
+'''
+
+buttonCommit=Button(root, height=1, width=10, text="Commit", command=lambda: [retrieve_input(), show_url_box()])
+buttonCommit.grid(row=1, column=1)
+
+
+lbl_url = Label(root)
+lbl_url.grid(row=0, column=0, sticky="NW")
+lbl_url.config(text="Insert URL here:")
+
+lbl_download = Label(root)
+lbl_download.grid(row=0, column=2, sticky="NE")
+lbl_download.config(text="Download Option:")
+
+lbl_status = Label(root)
+lbl_status.grid(row=4, column=0, sticky="W")
+lbl_status.config(text="Inserted URL:")
+
+lbl_files = Label(root)
+lbl_files.grid(row=2, column=0, sticky="W")
+lbl_files.config(text="Files:")
 
 root.mainloop()
