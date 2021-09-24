442faf985834db2903d09cda77a8284e80b8a20c
Frxhb <huber.frani@gmail.com>
2021-09-24 19:43:32 +0200

    edited loop in main func

--- youtube_dl_script.py
+++ youtube_dl_script.py
@@ -14,30 +14,18 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
-from functions_youtube import check_installation
-check_installation.all_func_check()
-
 def main_func():
+    from functions_youtube import check_installation
+    check_installation.all_func_check()
+
     type_abfrage.Type_Abfrage()
+
+    global ask_run
     ask_run = input ("Would you like to continue? Y/n:\n>>> ")
     if ask_run in ['yes', 'Yes', 'Y', 'y']:
-        ""
+        main_func()
     else:
-        print("okay program will close...")
+        print("Okay, close program now...")
         exit()
 
-global ask_re_run
-ask_re_run = input("Would you like to start? Y/n:\n>>> ")
-
-if ask_re_run == "y":
-    ask_re_run = True
-
-elif ask_re_run == "n":
-    ask_re_run = False
-
-while ask_re_run == "y":
-    main_func()
-else:
-    print("End..")
-    exit()
+main_func()
